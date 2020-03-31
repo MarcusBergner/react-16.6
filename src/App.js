ff7cc22946592d23d0167b52ad834a00f712e88a
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-31 07:21:34 +0200

    setting in app.js the new method react.lazy() for asynchron routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import Posts from "./containers/Posts";
+// import Posts from "./containers/Posts";
 import User from "./containers/User";
 import Welcome from "./containers/Welcome";
 
+const Posts = React.lazy(() => import("./containers/Posts"));
 /**
  * After React 16.6 came a new method to the react object. Lazy Method
  * which you can use to load your data, your components asynchronously.(Only when they are needed!)
@@ -20,7 +21,14 @@ class App extends Component {
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
+          <Route
+            path="/posts"
+            render={() => (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Posts />
+              </Suspense>
+            )}
+          />
         </React.Fragment>
       </BrowserRouter>
     );
