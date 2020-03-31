3dbb7bd2ca8e63d7427499156b3e2ee9f9247e19
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-31 06:38:10 +0200

    Explain the React.Lazy Method for new way of asynchron loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import Posts from './containers/Posts';
-import User from './containers/User';
-import Welcome from './containers/Welcome';
+import Posts from "./containers/Posts";
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
 
+/**
+ * After React 16.6 came a new method to the react object. Lazy Method
+ * which you can use to load your data, your components asynchronously.(Only when they are needed!)
+ */
 class App extends Component {
   render() {
     return (
