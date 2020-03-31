1db5283aff68b5ddd5f481739abd7338a55c45cc
marcus@NOTEBOOK <marcus11.04@web.de>
2020-03-31 07:31:49 +0200

    216 Lazy Loading with react.suspense after React 16.6

--- src/App.js
+++ src/App.js
@@ -9,28 +9,46 @@ const Posts = React.lazy(() => import("./containers/Posts"));
 /**
  * After React 16.6 came a new method to the react object. Lazy Method
  * which you can use to load your data, your components asynchronously.(Only when they are needed!)
+ * It is not supported for server-side-rendaring!
+ * Only for Client-side rendered applications!
  */
 class App extends Component {
+  state = { showPosts: false };
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { showPosts: !prevState.showPosts };
+    });
+  };
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route
-            path="/posts"
-            render={() => (
-              <Suspense fallback={<div>Loading...</div>}>
-                <Posts />
-              </Suspense>
-            )}
-          />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.modeHandler}>Toggle Mode</button>
+        {this.state.showPosts ? (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Posts />
+          </Suspense>
+        ) : (
+          <User />
+        )}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route
+      //       path="/posts"
+      //       render={() => (
+      //         <Suspense fallback={<div>Loading...</div>}>
+      //           <Posts />
+      //         </Suspense>
+      //       )}
+      //     />
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
